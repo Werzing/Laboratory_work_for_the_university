d0a413ba4359ed31e3572a48a64c494d5faae466
Werzing <dneprowsky2020@yandex.ru>
2026-02-15 11:15:52 +0300

    Добавлена функция для упорядочивания цветов российского флага

---
+++ Lab1/task2/task16_russian_flag.py
@@ -0,0 +1,19 @@
+def russian_flag_sort(colors):
+    # сортировка пузырьком
+    n = len(colors)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            # определяем вес цвета
+            def get_weight(c):
+                if c == 'белый':
+                    return 0
+                elif c == 'синий':
+                    return 1
+                else:
+                    return 2
+            if get_weight(colors[j]) > get_weight(colors[j+1]):
+                colors[j], colors[j+1] = colors[j+1], colors[j]
+    return colors
+
+arr = ["синий", "красный", "белый", "красный", "белый", "синий"]
+print(russian_flag_sort(arr))
