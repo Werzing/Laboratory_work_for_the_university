182383d07b38dfa08de6e1c714d9dc1fabe2f32a
Werzing <dneprowsky2020@yandex.ru>
2026-02-17 14:47:36 +0300

    Добавлена функция по поиску всех элементов перед последним минимальным

---
+++ Lab1/arrays/task9_before_last_min.py
@@ -0,0 +1,14 @@
+def before_last_min(arr):
+    if not arr:
+        return []
+    mn = arr[0]
+    for x in arr:
+        if x < mn:
+            mn = x
+    last_idx = -1
+    for i in range(len(arr)):
+        if arr[i] == mn:
+            last_idx = i
+    return arr[:last_idx]
+
+print(before_last_min([3,1,4,1,5]))
