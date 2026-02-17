e95551fd4aa83ce31a0f818b382198a7065d6273
Werzing <dneprowsky2020@yandex.ru>
2026-02-17 15:09:56 +0300

    Добавлена функция по поиску всех элементов после первого максимального

---
+++ Lab1/arrays/task21_after_first_max.py
@@ -0,0 +1,15 @@
+def after_first_max(arr):
+    if not arr:
+        return []
+    mx = arr[0]
+    for x in arr:
+        if x > mx:
+            mx = x
+    first_idx = -1
+    for i in range(len(arr)):
+        if arr[i] == mx:
+            first_idx = i
+            break
+    return arr[first_idx+1:]
+
+print(after_first_max([3,5,1,5,2]))
